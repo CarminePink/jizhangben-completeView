b40afa40441c283c0c776af52269c288a6f37a57
carmine <hl13886803841@sina.com>
2020-03-21 15:01:12 +0800

    deploy

---
+++ precache-manifest.6074d4fc84435a63d2e027bc832e58e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a188a8601e5dd713f4b",
+    "url": "/jizhangben-completeView/css/app.8a240a60.css"
+  },
+  {
+    "revision": "fc7ecc490e6e74008671984e5316da5a",
+    "url": "/jizhangben-completeView/index.html"
+  },
+  {
+    "revision": "9a188a8601e5dd713f4b",
+    "url": "/jizhangben-completeView/js/app.647d5ebe.js"
+  },
+  {
+    "revision": "7ae599f14136dd9dc0d5",
+    "url": "/jizhangben-completeView/js/chunk-vendors.3111d644.js"
+  },
+  {
+    "revision": "da70c26f8a53aefe434ed7f7a256d118",
+    "url": "/jizhangben-completeView/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jizhangben-completeView/robots.txt"
+  }
+]);
